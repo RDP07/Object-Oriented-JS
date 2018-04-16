ab973b9fee5cb8e77c9b55079cc93bfbfaaa3d1e
Ross DuPaw <rdupaw@drivindealer.com>
2018-04-16 14:12:43 -0500

    Test class and instance inhearitance

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
-console.log('in app.js')
+class Drone {
+  constructor (id, name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  static getCompany() {
+    console.log('in getCompany()')
+  }
+
+  fly () {
+    console.log('Drone ' + this.id + ' is flying')
+  }
+}
+
+let drone = new Drone('A123', 'Flyer')
+let drone2 = new Drone('B456', 'Twirl')
+
+drone.fly()
+drone2.fly()
+Drone.getCompany()
